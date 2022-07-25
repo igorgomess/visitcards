dca287ffa0756da2b0112d169772dd8610f8c8e5
Igor Gomes Silva <igor_gomes2001@outlook.com>
2022-07-25 14:57:27 -0300

    project-feature-settings

---
+++ Pods/Target Support Files/Pods-VisitCardsEstudo-VisitCardsEstudoUITests/Pods-VisitCardsEstudo-VisitCardsEstudoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VisitCardsEstudo_VisitCardsEstudoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_VisitCardsEstudo_VisitCardsEstudoUITests
+@end
--- VisitCardsEstudo/ViewController.swift
+++ VisitCardsEstudo/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "salveeee"
+        label.font = UIFont.systemFont(ofSize: 18.0)
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 16.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    // MARK: - Initializers
+
+    init () {
+        super.init(nibName: nil, bundle: nil)
+        setupViewHierarchy()
+        setupConstraints()
     }
 
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
+    // MARK: - Override functions
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+    // MARK: - Private functions
+
+    private func setupViewHierarchy() {
+        view.addSubview(nameLabel)
+
+    }
+
+    private func setupConstraints() {
+        nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12.0).isActive = true
+}
 }
 
