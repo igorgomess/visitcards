043330d4d09fff1872d6b371026a199fc1a7635f
Igor Gomes Silva <igor_gomes2001@outlook.com>
2022-07-25 21:19:35 -0300

    project-updates

--- VisitCardsEstudo/ViewController.swift
+++ VisitCardsEstudo/ViewController.swift
@@ -11,13 +11,42 @@ class ViewController: UIViewController {
 
     let nameLabel: UILabel = {
         let label = UILabel()
-        label.text = "salveeee"
+        label.text = "Igor Gomes"
         label.font = UIFont.systemFont(ofSize: 18.0)
         label.textColor = .black
         label.font = UIFont.boldSystemFont(ofSize: 16.0)
         label.translatesAutoresizingMaskIntoConstraints = false
+
         return label
     }()
+
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Developer iOS"
+        label.font = UIFont.systemFont(ofSize: 18.0)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let phoneLabel: UILabel = {
+        let label = UILabel()
+        label.text = "(11)91111-1111"
+        label.font = UIFont.systemFont(ofSize: 18.0)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let emailLabel: UILabel = {
+        let label = UILabel()
+        label.text = "igor_gomes2001@outlook.com"
+        label.font = UIFont.systemFont(ofSize: 18.0)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
 
     // MARK: - Initializers
 
@@ -43,12 +72,26 @@ class ViewController: UIViewController {
 
     private func setupViewHierarchy() {
         view.addSubview(nameLabel)
+        view.addSubview(titleLabel)
+        view.addSubview(phoneLabel)
+        view.addSubview(emailLabel)
+
+
 
     }
 
     private func setupConstraints() {
-        nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 300.0).isActive = true
         nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12.0).isActive = true
+
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 316.0).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12.0).isActive = true
+
+        phoneLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 340.0).isActive = true
+        phoneLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 11.0).isActive = true
+
+        emailLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 359.0).isActive = true
+        emailLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12.0).isActive = true
 }
 }
 
